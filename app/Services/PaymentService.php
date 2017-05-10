80769a5e7555a5939f1b76ae6de00acf277783e6
milos01 <milosa942@gmail.com>
2017-05-10 20:10:38 +0200

    Extent time of expired subscribed website #1

--- app/Services/PaymentService.php
+++ app/Services/PaymentService.php
@@ -5,6 +5,7 @@ namespace App\Services;
 use App\User;
 use App\Http\Controllers\Helpers\UserHelper as Usr;
 use App\Http\Controllers\Helpers\BraintreeHelper as Brain3;
+use Carbon\Carbon;
 
 
 class PaymentService
@@ -18,10 +19,19 @@ class PaymentService
     }
 
     public function automatedPaymentExpired($userInfo){
-
+        $now = Carbon::now();
+        $nextMonthAhead = $now->addMonth();
         foreach ($userInfo as $key => $info) {
             $user = $this->findUserById($info['id']);
             $result = $this->b3Sale($user, $info['totalPrice'], false, false);
+            if ($result->success) {
+                foreach ($user->websites as $key => $website) {
+                    if($website->expire_at && $now->diffInDays($website->expire_at, false) <= 0){
+                        $website->expire_at = $nextMonthAhead;
+                        $website->save();
+                    }
+                }
+            }
         }
         echo "Monthly subscription payment executed \n";
     }
